9b5089b85523a01d41a298d04bdb9e1cf6dc9781
dcox298 <davtcox@hotmail.com>
2024-03-16 16:12:45 -0500

    Init

---
+++ src/webparts/wastelandWarefare/components/IWastelandWarefareProps.ts
@@ -0,0 +1,7 @@
+export interface IWastelandWarefareProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
